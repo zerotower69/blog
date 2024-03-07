9fc4ea81c81b5c2530e037e2998a611b4cb29b4d
吴根荃 <wgq@geniuses.com.cn>
2024-03-07 15:12:27 +0800

    feat: [admin]添加管理端

---
+++ apps/backend/src/user/role.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/sequelize";
+import {PermissionModel, RoleModel} from "../models";
+import {Op} from "sequelize"
+
+@Injectable()
+export class RoleService {
+    constructor(
+        @InjectModel(RoleModel)
+        private roleModel:typeof RoleModel,
+    ) {
+
+    }
+    async findRolesByIds(roleIds:number[]){
+        return this.roleModel.findAll({
+            where:{
+                id:{
+                    [Op.in]:roleIds
+                }
+            },
+            attributes:{
+                exclude:[]
+            },
+            include:[{
+                model:PermissionModel,
+                through:{attributes:[]}
+            }]
+        })
+    }
+}
